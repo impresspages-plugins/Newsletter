409f180752ebf8d5fd7b277bd77a01bf9fc52077
Mangirdas Skripka <mangirdas@impresspages.org>
2015-03-26 23:33:07 +0200

    pass more data to view

--- Event.php
+++ Event.php
@@ -20,29 +20,31 @@ class Event
     public static function Newsletter_subscriberAdded($info)
     {
         if (ipGetOption('Newsletter.sendWelcomeEmail') && !ipGetOption('Newsletter.confirmSubscribers')) {
-            self::sendWelcomeEmail($info['email']);
+            self::sendWelcomeEmail($info);
         }
     }
 
     public static function Newsletter_subscriberConfirmed($info)
     {
         if (ipGetOption('Newsletter.sendWelcomeEmail')) {
-            self::sendWelcomeEmail($info['email']);
+            self::sendWelcomeEmail($info);
         }
     }
 
-    protected static function sendWelcomeEmail($email)
+    protected static function sendWelcomeEmail($info)
     {
 
         //send welcome email
         $message = ipGetOption('Newsletter.welcomeEmailText');
-        $emailHtml = ipView('view/welcomeEmail.php', array("content" => $message))->render();
+        $variables = array("content" => $message);
+        $variables = array_merge($variables, $info);
+        $emailHtml = ipView('view/welcomeEmail.php', $variables)->render();
 
         ipSendEmail(
             ipGetOption('Newsletter.fromEmail'),
             ipGetOption('Newsletter.fromName'),
-            $email,
-            $email,
+            $info['email'],
+            $info['email'],
             ipGetOption('Newsletter.welcomeEmailSubject'),
             $emailHtml
         );
